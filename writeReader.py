b9183a5df894f33a3b253c9908493300891e14f6
DutchNukeTD <105785047+DutchNukeTD@users.noreply.github.com>
2023-04-05 13:30:28 +0200

    Add files via upload

---
+++ writeReader.py
@@ -0,0 +1,46 @@
+# Script Golan
+# 20220323
+# keyboard shortcut for inport write nodes render 
+# "shift+r"
+
+import nuke 
+
+def writeRender():
+
+    try:
+        write = nuke.selectedNode()
+        xpos = write['xpos'].value()
+        ypos = write['ypos'].value()
+        ypos = ypos + 100
+        writeFile = write['file'].value()
+        writeName = writeFile.split("/")[-1]
+    
+        # Een lijst gemaakt met meerdere variaties van mogelijke bestandsnamen.
+        # Soms werkt het alleen met 1 hashtag en de andere keer met 4.
+        nameList = []
+        writeNameFixed = writeName.replace("%04d", "#")
+        writeNameFixedTwo = writeName.replace("%04d", "####")
+        nameList.append(writeNameFixed)
+        nameList.append(writeNameFixedTwo)
+    
+        writeFolder = writeFile.split(writeName)[0]
+        writeColorspace = write['colorspace'].value()
+    
+        
+        for seq in nuke.getFileNameList(writeFolder):
+            for name in nameList:
+                if seq.split()[0] == name:
+                    readNode = nuke.createNode('Read')
+                    readNode['file'].fromUserText(writeFolder + seq)
+                    readNode['colorspace'].setValue(writeColorspace)
+                    readNode['xpos'].setValue(xpos)
+                    readNode['ypos'].setValue(ypos)
+
+    except:
+        pass
+        
+writeRender()
+
+
+
+
